45b4349acd47a971cec2759619c2a8568ca7b471
REGIS <23308060610258@cetis61.edu.mx>
2026-03-03 10:11:56 -0500

    complete el trabajo

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import flet as ft
 def main(page: ft.Page):
-    page.title = "Formulario de Registro del Eventos"
+    page.title = "Formulario de registro del eventos"
     txt_nombre = ft.TextField(
-        label="Nombre del evento",
+        label="nombre del evento",
         width=400
     )
     dropdown_tipo = ft.Dropdown(
-        label="Tipo de evento",
+        label="tipo de evento",
         width=300,
         options=[
-            ft.dropdown.Option("Conferencia"),
-            ft.dropdown.Option("Taller"),
-            ft.dropdown.Option("Reunión"),
+            ft.dropdown.Option("conferencia"),
+            ft.dropdown.Option("taller"),
+            ft.dropdown.Option("reunión"),
         ]
     )
     radio_modalidad = ft.RadioGroup(
         content=ft.Row([
-            ft.Radio(value="Presencial", label="Presencial"),
-            ft.Radio(value="Virtual", label="Virtual"),
+            ft.Radio(value="presencial", label="presencial"),
+            ft.Radio(value="virtual", label="virtual"),
         ])
     )
     check_inscripcion = ft.Checkbox(
@@ -50,11 +50,11 @@ def main(page: ft.Page):
         txt_resumen.value = resumen
         page.update()
     btn_resumen = ft.ElevatedButton(
-        content=ft.Text("Mostrar resumen"),
+        content=ft.Text("mostrar resumen"),
         on_click=mostrar_resumen
 )
     page.add(
-        ft.Text("Registro de Evento", size=24, weight="bold"),
+        ft.Text("Registro de evento", size=24, weight="bold"),
         txt_nombre,
         dropdown_tipo,
         ft.Text("Modalidad:"),
